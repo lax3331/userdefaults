e470e0fe73b5b6c2455dd27e30301110343935df
Nikky <la2life1234@gmail.com>
2023-05-08 15:02:08 +0300

    add filemanager

--- userdefaults/Model.swift
+++ userdefaults/Model.swift
@@ -13,6 +13,7 @@ class Model {
     struct Const {
         static var defaultFont: Double = 17.0
         static var defaultTheme: Bool = false
+        static let fileName = "fileName.txt"
         
         enum keys: String {
             case fontKey = "fontKey"
@@ -57,16 +58,37 @@ class Model {
             break
         }
         if let data = try? JSONEncoder().encode(userDataStruct) {
-            UserDefaults.standard.set(data, forKey: Const.keys.userData.rawValue) // Здесь была ошибка: нужно сохранять data в UserDefaults, а не element
+//            UserDefaults.standard.set(data, forKey: Const.keys.userData.rawValue)
+            try! data.write(to: getFile(name: Model.Const.fileName))
+            print("File created")
         }
     }
     
     private func get() -> UserData {
-        if let data = UserDefaults.standard.object(forKey: Const.keys.userData.rawValue) as? Data { // Проверяем, что данные, которые мы получили из UserDefaults, можно преобразовать в Data
+//        if let data = UserDefaults.standard.object(forKey: Const.keys.userData.rawValue) as? Data
+        if let data = try? Data(contentsOf: getFile(name: Model.Const.fileName)){
             if let dataUD = try? JSONDecoder().decode(UserData.self, from: data) {
                 userDataStruct = dataUD
+                print("Data read")
             }
         }
         return userDataStruct
     }
+    private func getFile(name: String) -> URL {
+        var path = try! FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: true)
+        path.appendPathComponent(name)
+        fileDetail(named: path)
+        return path
+    }
+    
+    private func fileDetail(named: URL) {
+        let keys: Set<URLResourceKey> = [.creationDateKey, .fileSizeKey, .nameKey]
+        let resourseValue = try! named.resourceValues(forKeys: keys)
+        print(
+            "\(resourseValue.creationDate)",
+            "\(resourseValue.fileSize)",
+            "\(resourseValue.name)"
+        )
+    }
+    
 }
